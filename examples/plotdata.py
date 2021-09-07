2b9bacc3bdfcac524333486eaec5f2416e4a2182
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2021-09-07 14:52:25 +0900

    plot data example

---
+++ examples/plotdata.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from magicclass import magicclass
+from pathlib import Path
+
+@magicclass
+class PlotData:
+    """
+    Load 1D data and plot it.
+    """
+    def __init__(self, title=None):
+        self.title = title
+        self.data = None
+        self.path = None
+    
+    def random_data(self, loc=0.0, scale=1.0, size=100):
+        """
+        Generate random data.
+        """        
+        self.data = np.random.normal(loc=loc, scale=scale, size=size)
+        
+    def load(self, path:Path):
+        """
+        Load file.
+        """
+        self.path = str(path)
+        self.data = np.loadtxt(path)
+        
+    def plot(self):
+        """
+        Plot data.
+        """
+        if self.title:
+            plt.title(self.title)
+        plt.plot(self.data)
+        plt.show()
+
+plotdata = PlotData()
+plotdata.show()
