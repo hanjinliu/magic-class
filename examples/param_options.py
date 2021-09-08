f45767f1c3b21b824bd27547bad9933394a83281
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2021-09-09 00:24:50 +0900

    param option example

---
+++ examples/param_options.py
@@ -0,0 +1,21 @@
+from magicclass import magicclass, set_options
+
+@magicclass
+class Main:
+    @set_options(x={"widget_type": "FloatSlider", 
+                    "min": -1, 
+                    "max": 1, 
+                    "step": 0.1}
+                 )
+    def print_float(self, x=0):
+        print(x)
+    
+    @set_options(c={"widget_type": "RadioButtons", 
+                    "choices": ["first", "second", "third"],
+                    "value": "first"
+                    }
+                 )
+    def choose_one(self, c):
+        print(c)
+
+Main().show()
