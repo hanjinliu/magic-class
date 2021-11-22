b9c59723322675102bc01c2ea3a8505fe72e7975
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2021-11-22 21:45:28 +0900

    docs and threading

--- magicclass/widgets/pywidgets/list.py
+++ magicclass/widgets/pywidgets/list.py
@@ -43,14 +43,26 @@ In a similar way, you can dispatch display method and context menu.
 """
 
 from __future__ import annotations
-from typing import Any
+from typing import Any, Iterable
 from collections.abc import MutableSequence
 from qtpy.QtWidgets import QListWidget, QListWidgetItem, QAbstractItemView
 
 from .object import BaseWidget, ContextMenuMixin, PyObjectBound
     
 class ListWidget(BaseWidget, MutableSequence):
-    def __init__(self, value=None, dragdrop: bool = True, **kwargs):
+    def __init__(self, value: Iterable[Any] = None, dragdrop: bool = True, **kwargs):
+        """
+        A widget composed of a list of items. This widget behaves very similar to Python
+        list so that you can append, insert, pop or do any indexing.
+
+        Parameters
+        ----------
+        value : Iterable, optional
+            Initial value of the list widget. Any iterable object that can be passed to
+            Python list.
+        dragdrop : bool, default is True
+            Allow drag and drop of list contents.
+        """        
         super().__init__(**kwargs)
         self._listwidget = PyListWidget(self.native)
         self._listwidget.setParentWidget(self)
@@ -80,15 +92,26 @@ class ListWidget(BaseWidget, MutableSequence):
                 self.append(v)
         
     def __len__(self) -> int:
+        """
+        Length of widget contents.
+        """        
         return self._listwidget.count()
     
     @property
     def value(self) -> list[Any]:
+        """
+        Get all the contents as a Python list.
+
+        Returns
+        -------
+        list
+            Contents of the list widget.
+        """        
         return [self._listwidget.item(i).obj for i in range(len(self))]
             
     def insert(self, i: int, obj: Any):
         """
-        Insert any item to the list.
+        Insert object of any type to the list.
         """
         name = self._delegates.get(type(obj), str)(obj)
         item = PyListWidgetItem(self._listwidget, obj=obj, name=name)
@@ -96,15 +119,21 @@ class ListWidget(BaseWidget, MutableSequence):
         
     def __getitem__(self, i: int) -> Any:
         """
-        Get i-th python object.
+        Get i-th Python object (not widget item object!).
         """        
         return self._listwidget.item(i).obj
     
     def __setitem__(self, i: int, obj: Any):
+        """
+        Set i-th Python object.
+        """        
         self._listwidget.takeItem(i)
         self.insert(i, obj)
     
     def __delitem__(self, i: int):
+        """
+        Delete i-th Python object from the list widget.
+        """        
         self._listwidget.takeItem(i)
 
     def clear(self):
@@ -113,6 +142,49 @@ class ListWidget(BaseWidget, MutableSequence):
         """
         self._listwidget.clear()
     
+    def index(self, obj: Any, start: int = 0, stop: int = None) -> int:
+        """
+        Find object or list widget item from the list widget.
+
+        Parameters
+        ----------
+        obj : Any
+            Object to find. If a PyListWidgetItem is given, the index of the item
+            (not the tagged object) is searched for.
+        start : int, optional
+            Starting index, by default 0
+        stop : int, optional
+            Index to stop searching.
+
+        Returns
+        -------
+        int
+            Index of object.
+
+        Raises
+        ------
+        ValueError
+            If object was not found.
+        """        
+        if isinstance(obj, PyListWidgetItem):
+            f = self._listwidget.item
+        else:
+            f = lambda i: self._listwidget.item(i).obj
+        if start is not None and start < 0:
+            start = max(len(self) + start, 0)
+        if stop is not None and stop < 0:
+            stop += len(self)
+
+        i = start
+        while stop is None or i < stop:
+            try:
+                v = f(i)
+                if v is obj or v == obj:
+                    return i
+            except IndexError:
+                break
+            i += 1
+        raise ValueError(f"Object {obj!r} not found in the list")
 
 
 class PyListWidget(ContextMenuMixin, QListWidget):
--- magicclass/widgets/threading.py
+++ magicclass/widgets/threading.py
@@ -2,10 +2,11 @@ from __future__ import annotations
 from functools import partial, wraps
 import time
 import threading
-from typing import Callable, Iterable
+from typing import Callable, Iterable, TypeVar
 
 from magicgui.widgets import Container, Label, ProgressBar
 
+T = TypeVar("T")
 
 class ProgressWidget(Container):
     def __init__(self, text: str = None, visible: bool = False):
@@ -17,39 +18,44 @@ class ProgressWidget(Container):
                          labels=False, 
                          visible=visible)
     
-    def __call__(self, iterable: Iterable):
+    def __call__(self, iterable: Iterable[T]) -> Iterable[T]:
         if hasattr(iterable, "__len__"):
             dt = self._pbar.max/len(iterable)
         else:
             dt = 0
             
-        with self.run(infinite = dt==0) as p:
+        with self.run() as p:
             for it in iterable:
                 yield it
-                if dt > 0:
-                    p._pbar.increment(dt)
-            if dt > 0:
-                p._pbar.value = self._pbar.max
+                p._pbar.increment(dt)
     
     def range(self, n: int):
         return self(range(n))
     
-    def run(self, infinite: bool = True):
-        return progress(infinite=infinite, progress=self)
+    def run(self) -> progress:
+        """
+        Build a context that progress bar will be displayed.
+
+        Returns
+        -------
+        progress
+            Context manager for progress bar.
+        """        
+        return progress(progress=self)
     
-    def show_progress(self, f: Callable = None):
+    def show_progress(self, f: Callable = None) -> Callable:
         @wraps(f)
         def function_with_progressbar(*args, **kwargs):
-            with self.run(infinite=True):
+            with self.run():
                 out = f(*args, **kwargs)
             return out
         return function_with_progressbar
     
-    def _as_infinite(self):
-        self._pbar.value = 0
-        self._pbar.min = 0
-        self._pbar.max = 0
-        
+    def _show_all(self):
+        self.visible = True
+        self._pbar.visible = True
+        self._description.visible = True
+            
     @property
     def text(self) -> str:
         return self._description.value
@@ -66,25 +72,25 @@ class ProgressWidget(Container):
     def value(self, v: int):
         self._pbar.value = min(v, self._pbar.max)
 
+_progress_widget = ProgressWidget()
+_progress_widget.visible = False
+
 class progress:
-    def __init__(self, function: Callable = None, *, infinite: bool = True, 
-                 progress: ProgressWidget = None):
+    def __init__(self, obj: Callable = None, *, progress: ProgressWidget = None):
         if progress is None:
-            progress = ProgressWidget()
             self.close_on_finish = True
+            progress = _progress_widget
         else:
             self.close_on_finish = False
         
-        if function is not None:
-            function = self._wrap_function(function)
-        
-        self.function = function
         self.progress = progress
-        self.pbar_was_visible = progress.visible
-        self.infinite = infinite
-        self.stop_event = None
         self.thread = None
         self.running = False
+        
+        self.function = None
+        if isinstance(obj, Callable):
+            self.function = self._wrap_function(obj)
+        
     
     @property
     def function(self):
@@ -100,7 +106,7 @@ class progress:
     @property
     def __signature__(self):
         return self.function.__signature__
-    
+        
     def __call__(self, *args, **kwargs):
         if self.function is None:
             if len(args) != 1 or not callable(args[0]):
@@ -112,7 +118,7 @@ class progress:
     
     def __get__(self, obj, objtype=None):
         f = partial(self.function, obj)
-        return self.__class__(f, infinite=self.infinite, progress=self.progress)
+        return self.__class__(f, progress=self.progress)
         
     def _wrap_function(self, function: Callable):
         @wraps(function)
@@ -125,31 +131,28 @@ class progress:
     def _run(self):
         self.running = True
         
-        if self.infinite:
-            self.progress._as_infinite()
         while self.running:
-            time.sleep(0.1)
+            time.sleep(0.01)
+        
+        # Show 100%
+        self.progress.value = self.progress._pbar.max
+        time.sleep(0.1)
     
-    def __enter__(self):
+    def __enter__(self) -> ProgressWidget:
+        self.pbar_was_visible = self.progress.visible
+        self.progress._show_all()
+        self.progress.value = 0
         self.thread = threading.Thread(target=self._run)
         self.thread.start()
-        self.pbar_was_visible = self.progress.visible
-        self.progress.visible = True
-        self.progress.show()
-        self.progress.value = 0
         
         return self.progress
 
-    def __exit__(self, exc_type, exc_value, traceback):
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
         self.running = False
         if self.thread is not None:
             self.thread.join()
         
-        if self.close_on_finish:
-            self.progress.close()
-            del self.progress
-        else:
-            self.progress._pbar.min = 0
-            self.progress._pbar.max = 1000
-            self.progress._pbar.value = 1000
-            self.progress.visible = self.pbar_was_visible
+        self.progress._pbar.min = 0
+        self.progress._pbar.max = 1000
+        self.progress._pbar.value = 1000
+        self.progress.visible = self.pbar_was_visible
