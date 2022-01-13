46faff2df5594fbaffcb9859a641e63d4bb44beb
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2022-01-14 00:17:38 +0900

    fix test

--- tests/test_custom_widgets.py
+++ tests/test_custom_widgets.py
@@ -7,13 +7,13 @@ def test_checkbutton():
         b = field(wdt.CheckButton)
         @set_options(x={"widget_type": wdt.CheckButton})
         def f(self, x: bool):
-            print(x)
+            pass
     ui = A()
     assert isinstance(ui.b, wdt.CheckButton)
-    ui.b.changed()
-    assert ui.b.value
-    ui.b.changed()
-    assert not ui.b.value
+    ui.b.value = True
+    assert ui.b.native.isChecked()
+    ui.b.value = False
+    assert not ui.b.native.isChecked()
     
     ui["f"].changed()
     assert isinstance(ui["f"].mgui[0], wdt.CheckButton)
