9e9bd86d7351a034b5a94b3c0428d82775f94570
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2022-03-08 20:08:16 +0900

    fix test

--- tests/test_types.py
+++ tests/test_types.py
@@ -57,3 +57,41 @@ def test_nested_types():
     assert isinstance(tuple_edit, widgets.TupleEdit)
     assert tuple_edit[0].value == ""
     assert tuple_edit[1].value == None
+
+def test_set_options():
+    @magicclass
+    class A:
+        @set_options(x={"text": "x-text", "options": {"min": -1}})
+        def f(self, x: Optional[int] = 0):
+            pass
+        @set_options(y={"layout": "vertical", "options": {"min": -1}})
+        def g(self, y: List[int] = (1,)):
+            pass
+        @set_options(z={"layout": "vertical", "options": {"min": -1}})
+        def h(self, z: Tuple[int, int] = (2, 2)):
+            pass
+
+    ui = A()
+    ui["f"].changed()
+    opt = ui["f"].mgui[0]
+    ui["g"].changed()
+    list_edit = ui["g"].mgui[0]
+    ui["h"].changed()
+    tuple_edit = ui["h"].mgui[0]
+
+    assert isinstance(opt, widgets.OptionalWidget)
+    assert opt.text == "x-text"
+    assert opt[1].visible
+    assert opt[1].min == -1
+
+    assert isinstance(list_edit, widgets.ListEdit)
+    assert list_edit.layout == "vertical"
+    assert list_edit[0].value == 1
+    assert list_edit[0].min == -1
+
+    assert isinstance(tuple_edit, widgets.TupleEdit)
+    assert tuple_edit.layout == "vertical"
+    assert tuple_edit[0].value == 2
+    assert tuple_edit[0].min == -1
+    assert tuple_edit[1].value == 2
+    assert tuple_edit[1].min == -1
