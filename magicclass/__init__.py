12e8707171ec3b63fb10b4f94fa2afc3e6594c3d
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2022-03-09 12:23:32 +0900

    better matplotlib backend in logger

--- magicclass/__init__.py
+++ magicclass/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.6.0.dev0"
+__version__ = "0.6.0.dev1"
 
 from .core import (
     magicclass,
--- magicclass/widgets/logger.py
+++ magicclass/widgets/logger.py
@@ -6,10 +6,23 @@ from qtpy import QtWidgets as QtW, QtGui
 from magicgui.backends._qtpy.widgets import QBaseWidget
 from magicgui.widgets import Widget
 import logging
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
+
+try:
+    import numpy as np
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+    from matplotlib.backends.backend_agg import FigureCanvasAgg
+
+    FigureCanvas = FigureCanvasAgg
+    MATPLOTLIB_AVAILABLE = True
+
+except ImportError:
+    MATPLOTLIB_AVAILABLE = False
 
 if TYPE_CHECKING:
     import numpy as np
+    from matplotlib.figure import Figure
 
 # See https://stackoverflow.com/questions/28655198/best-way-to-display-logs-in-pyqt
 
@@ -54,6 +67,9 @@ class QtLogger(QtW.QTextEdit):
         self.setTextCursor(cursor)
         return None
 
+    def _get_background_color(self) -> tuple[int, int, int, int]:
+        return self.palette().color(self.backgroundRole()).getRgb()
+
 
 class Logger(Widget, logging.Handler):
     """
@@ -87,7 +103,7 @@ class Logger(Widget, logging.Handler):
         logger.print("text")
 
         # a context manager that change the destination of print function.
-        with logger.set_as_stdout():
+        with logger.set_stdout():
             print("text")
             function_that_print_something()
 
@@ -98,12 +114,14 @@ class Logger(Widget, logging.Handler):
 
     .. code-block:: python
 
-        with logger.set_as_logger():
+        with logger.set_logger():
             function_that_log_something()
 
         logging.getLogger().addHandler(logger)
 
     """
+
+    current_logger: Logger | None = None
 
     def __init__(self):
         logging.Handler.__init__(self)
@@ -114,22 +132,35 @@ class Logger(Widget, logging.Handler):
         self.print(msg)
 
     def clear(self):
+        """Clear all the histories."""
         self._widget._qwidget.clear()
         return None
 
     def print(self, *msg, sep=" ", end="\n"):
+        """Print things in the end of the logger widget."""
         self._widget._qwidget.appendText(sep.join(map(str, msg)) + end)
         return None
 
     def print_html(self, html: str):
+        """Print things in the end of the logger widget as a HTML string."""
         self._widget._qwidget.appendHtml(html)
         return None
 
-    def print_table(self, table):
+    def print_table(self, table, index: bool = True):
+        """
+        Print object as a table in the logger widget.
+
+        Parameters
+        ----------
+        table : table-like object
+            Any object that can be passed to ``pandas.DataFrame`` can be used.
+        index : bool, default is True
+            Whether to show the index column.
+        """
         import pandas as pd
 
         df = pd.DataFrame(table)
-        self._widget._qwidget.appendHtml(df.to_html())
+        self._widget._qwidget.appendHtml(df.to_html(index=index))
         return None
 
     def print_image(
@@ -141,7 +172,7 @@ class Logger(Widget, logging.Handler):
         norm=None,
         width=None,
         height=None,
-    ):
+    ) -> None:
         from magicgui import _mpl_image
 
         img = _mpl_image.Image()
@@ -165,6 +196,13 @@ class Logger(Widget, logging.Handler):
         self._widget._qwidget.appendImage(image)
         return None
 
+    def print_figure(self, fig: Figure) -> None:
+        fig.canvas.draw()
+        data = np.asarray(fig.canvas.renderer.buffer_rgba(), dtype=np.uint8)
+        self.print_image(data)
+
+        return None
+
     def write(self, msg):
         self.print(msg, end="")
         return None
@@ -173,15 +211,73 @@ class Logger(Widget, logging.Handler):
         pass
 
     @contextmanager
-    def set_as_stdout(self):
+    def set_stdout(self):
         """A context manager for printing things in this widget."""
-        sys.stdout = self
-        yield self
-        sys.stdout = sys.__stdout__
+        try:
+            sys.stdout = self
+            yield self
+        finally:
+            sys.stdout = sys.__stdout__
 
     @contextmanager
-    def set_as_logger(self):
+    def set_logger(self):
         """A context manager for logging things in this widget."""
-        logging.getLogger().addHandler(self)
-        yield self
-        logging.getLogger().removeHandler(self)
+        try:
+            logging.getLogger().addHandler(self)
+            yield self
+        finally:
+            logging.getLogger().removeHandler(self)
+
+    @contextmanager
+    def set_plt(self, style=None):
+        """A context manager for inline plot in the logger widget."""
+        if not MATPLOTLIB_AVAILABLE:
+            yield self
+            return None
+        self.__class__.current_logger = self
+        if style is None:
+            style = self._get_proper_plt_style()
+        backend = mpl.get_backend()
+        show._called = False
+        try:
+            mpl.use("module://magicclass.widgets.logger")
+            with plt.style.context(style):
+                yield self
+        finally:
+            if not show._called:
+                show()
+            self.__class__.current_logger = None
+            mpl.use(backend)
+        return None
+
+    def _get_proper_plt_style(self) -> dict[str, Any]:
+        color = self._widget._qwidget._get_background_color()
+        is_dark = sum(color[:3]) < 382.5
+        if is_dark:
+            params = plt.style.library["dark_background"]
+        else:
+            keys = plt.style.library["dark_background"].keys()
+            with plt.style.context("default"):
+                params: dict[str, Any] = {}
+                rcparams = plt.rcParams
+                for key in keys:
+                    params[key] = rcparams[key]
+        params["figure.facecolor"] = "#00000000"
+        params["axes.facecolor"] = "#00000000"
+        return params
+
+
+# The plt.show function will be overwriten to this.
+# Modified from matplotlib_inline (BSD 3-Clause "New" or "Revised" License)
+# https://github.com/ipython/matplotlib-inline
+def show(close=True, block=None):
+    logger = Logger.current_logger
+    from matplotlib._pylab_helpers import Gcf
+
+    try:
+        for figure_manager in Gcf.get_all_fig_managers():
+            logger.print_figure(figure_manager)
+    finally:
+        show._called = True
+        if close and Gcf.get_all_fig_managers():
+            plt.close("all")
