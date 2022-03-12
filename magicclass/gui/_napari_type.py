10af4a970420fc9ac1197fa96e04be5c799b5c68
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2022-03-12 11:39:06 +0900

    napari viewer macro bug fix

--- magicclass/gui/_napari_type.py
+++ magicclass/gui/_napari_type.py
@@ -1,13 +1,13 @@
-from napari import Viewer, current_viewer
+from napari import Viewer
 from napari.layers import Layer
-from macrokit import register_type, symbol, Symbol, Expr
+from macrokit import register_type, Symbol, Expr
 
+VIEWER_SYMBOL = Symbol.var("viewer")
 
-register_type(Viewer, lambda viewer: Symbol("viewer", id(viewer)))
+register_type(Viewer, lambda viewer: VIEWER_SYMBOL)
 
 
 @register_type(Layer)
 def _get_layer(layer: Layer):
-    vw = symbol(current_viewer())
-    expr = Expr("getitem", [Expr("getattr", [vw, "layers"]), layer.name])
+    expr = Expr("getitem", [Expr("getattr", [VIEWER_SYMBOL, "layers"]), layer.name])
     return expr
