167520e1b7aba1ff248c8758240ca1e12edc5856
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2022-03-12 11:20:23 +0900

    napari viewer macro bug fix

--- magicclass/gui/_napari_type.py
+++ magicclass/gui/_napari_type.py
@@ -1,9 +1,9 @@
 from napari import Viewer, current_viewer
 from napari.layers import Layer
-from macrokit import register_type, symbol, Expr
+from macrokit import register_type, symbol, Symbol, Expr
 
 
-register_type(Viewer, lambda e: "viewer")
+register_type(Viewer, lambda viewer: Symbol("viewer", id(viewer)))
 
 
 @register_type(Layer)
