08fe3095a3ee67e10ed62d9daf53fabe18490487
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2022-07-06 11:51:22 +0900

    fix icon object for older qt

--- magicclass/_gui/_icon.py
+++ magicclass/_gui/_icon.py
@@ -103,6 +103,16 @@ def get_icon(val: Any) -> _IconBase:
     return icon
 
 
+class _StandardPixmap:
+    """To avoid version dependency."""
+
+    def __getattr__(self, name):
+        return getattr(QStyle.StandardPixmap, name, None)
+
+
+sp = _StandardPixmap()
+
+
 class Icon(SimpleNamespace):
     """
     Namespace of icons.
@@ -120,83 +130,83 @@ class Icon(SimpleNamespace):
     """
 
     # fmt: off
-    TitleBarMenuButton = QStyle.StandardPixmap.SP_TitleBarMenuButton
-    TitleBarMinButton = QStyle.StandardPixmap.SP_TitleBarMinButton
-    TitleBarMaxButton = QStyle.StandardPixmap.SP_TitleBarMaxButton
-    TitleBarCloseButton = QStyle.StandardPixmap.SP_TitleBarCloseButton
-    TitleBarNormalButton = QStyle.StandardPixmap.SP_TitleBarNormalButton
-    TitleBarShadeButton = QStyle.StandardPixmap.SP_TitleBarShadeButton
-    TitleBarUnshadeButton = QStyle.StandardPixmap.SP_TitleBarUnshadeButton
-    TitleBarContextHelpButton = QStyle.StandardPixmap.SP_TitleBarContextHelpButton
-    MessageBoxInformation = QStyle.StandardPixmap.SP_MessageBoxInformation
-    MessageBoxWarning = QStyle.StandardPixmap.SP_MessageBoxWarning
-    MessageBoxCritical = QStyle.StandardPixmap.SP_MessageBoxCritical
-    MessageBoxQuestion = QStyle.StandardPixmap.SP_MessageBoxQuestion
-    DockWidgetCloseButton = QStyle.StandardPixmap.SP_DockWidgetCloseButton
-    DesktopIcon = QStyle.StandardPixmap.SP_DesktopIcon
-    TrashIcon = QStyle.StandardPixmap.SP_TrashIcon
-    ComputerIcon = QStyle.StandardPixmap.SP_ComputerIcon
-    DriveFDIcon = QStyle.StandardPixmap.SP_DriveFDIcon
-    DriveHDIcon = QStyle.StandardPixmap.SP_DriveHDIcon
-    DriveCDIcon = QStyle.StandardPixmap.SP_DriveCDIcon
-    DriveDVDIcon = QStyle.StandardPixmap.SP_DriveDVDIcon
-    DriveNetIcon = QStyle.StandardPixmap.SP_DriveNetIcon
-    DirOpenIcon = QStyle.StandardPixmap.SP_DirOpenIcon
-    DirClosedIcon = QStyle.StandardPixmap.SP_DirClosedIcon
-    DirLinkIcon = QStyle.StandardPixmap.SP_DirLinkIcon
-    FileIcon = QStyle.StandardPixmap.SP_FileIcon
-    FileLinkIcon = QStyle.StandardPixmap.SP_FileLinkIcon
-    ToolBarHorizontalExtensionButton = QStyle.StandardPixmap.SP_ToolBarHorizontalExtensionButton
-    ToolBarVerticalExtensionButton = QStyle.StandardPixmap.SP_ToolBarVerticalExtensionButton
-    FileDialogStart = QStyle.StandardPixmap.SP_FileDialogStart
-    FileDialogEnd = QStyle.StandardPixmap.SP_FileDialogEnd
-    FileDialogToParent = QStyle.StandardPixmap.SP_FileDialogToParent
-    FileDialogNewFolder = QStyle.StandardPixmap.SP_FileDialogNewFolder
-    FileDialogDetailedView = QStyle.StandardPixmap.SP_FileDialogDetailedView
-    FileDialogInfoView = QStyle.StandardPixmap.SP_FileDialogInfoView
-    FileDialogContentsView = QStyle.StandardPixmap.SP_FileDialogContentsView
-    FileDialogListView = QStyle.StandardPixmap.SP_FileDialogListView
-    FileDialogBack = QStyle.StandardPixmap.SP_FileDialogBack
-    DirIcon = QStyle.StandardPixmap.SP_DirIcon
-    DialogOkButton = QStyle.StandardPixmap.SP_DialogOkButton
-    DialogCancelButton = QStyle.StandardPixmap.SP_DialogCancelButton
-    DialogHelpButton = QStyle.StandardPixmap.SP_DialogHelpButton
-    DialogOpenButton = QStyle.StandardPixmap.SP_DialogOpenButton
-    DialogSaveButton = QStyle.StandardPixmap.SP_DialogSaveButton
-    DialogCloseButton = QStyle.StandardPixmap.SP_DialogCloseButton
-    DialogApplyButton = QStyle.StandardPixmap.SP_DialogApplyButton
-    DialogResetButton = QStyle.StandardPixmap.SP_DialogResetButton
-    DialogDiscardButton = QStyle.StandardPixmap.SP_DialogDiscardButton
-    DialogYesButton = QStyle.StandardPixmap.SP_DialogYesButton
-    DialogNoButton = QStyle.StandardPixmap.SP_DialogNoButton
-    ArrowUp = QStyle.StandardPixmap.SP_ArrowUp
-    ArrowDown = QStyle.StandardPixmap.SP_ArrowDown
-    ArrowLeft = QStyle.StandardPixmap.SP_ArrowLeft
-    ArrowRight = QStyle.StandardPixmap.SP_ArrowRight
-    ArrowBack = QStyle.StandardPixmap.SP_ArrowBack
-    ArrowForward = QStyle.StandardPixmap.SP_ArrowForward
-    DirHomeIcon = QStyle.StandardPixmap.SP_DirHomeIcon
-    CommandLink = QStyle.StandardPixmap.SP_CommandLink
-    VistaShield = QStyle.StandardPixmap.SP_VistaShield
-    BrowserReload = QStyle.StandardPixmap.SP_BrowserReload
-    BrowserStop = QStyle.StandardPixmap.SP_BrowserStop
-    MediaPlay = QStyle.StandardPixmap.SP_MediaPlay
-    MediaStop = QStyle.StandardPixmap.SP_MediaStop
-    MediaPause = QStyle.StandardPixmap.SP_MediaPause
-    MediaSkipForward = QStyle.StandardPixmap.SP_MediaSkipForward
-    MediaSkipBackward = QStyle.StandardPixmap.SP_MediaSkipBackward
-    MediaSeekForward = QStyle.StandardPixmap.SP_MediaSeekForward
-    MediaSeekBackward = QStyle.StandardPixmap.SP_MediaSeekBackward
-    MediaVolume = QStyle.StandardPixmap.SP_MediaVolume
-    MediaVolumeMuted = QStyle.StandardPixmap.SP_MediaVolumeMuted
-    DirLinkOpenIcon = QStyle.StandardPixmap.SP_DirLinkOpenIcon
-    LineEditClearButton = QStyle.StandardPixmap.SP_LineEditClearButton
-    DialogYesToAllButton = QStyle.StandardPixmap.SP_DialogYesToAllButton
-    DialogNoToAllButton = QStyle.StandardPixmap.SP_DialogNoToAllButton
-    DialogSaveAllButton = QStyle.StandardPixmap.SP_DialogSaveAllButton
-    DialogAbortButton = QStyle.StandardPixmap.SP_DialogAbortButton
-    DialogRetryButton = QStyle.StandardPixmap.SP_DialogRetryButton
-    DialogIgnoreButton = QStyle.StandardPixmap.SP_DialogIgnoreButton
-    RestoreDefaultsButton = QStyle.StandardPixmap.SP_RestoreDefaultsButton
-    CustomBase = QStyle.StandardPixmap.SP_CustomBase
+    TitleBarMenuButton = sp.SP_TitleBarMenuButton
+    TitleBarMinButton = sp.SP_TitleBarMinButton
+    TitleBarMaxButton = sp.SP_TitleBarMaxButton
+    TitleBarCloseButton = sp.SP_TitleBarCloseButton
+    TitleBarNormalButton = sp.SP_TitleBarNormalButton
+    TitleBarShadeButton = sp.SP_TitleBarShadeButton
+    TitleBarUnshadeButton = sp.SP_TitleBarUnshadeButton
+    TitleBarContextHelpButton = sp.SP_TitleBarContextHelpButton
+    MessageBoxInformation = sp.SP_MessageBoxInformation
+    MessageBoxWarning = sp.SP_MessageBoxWarning
+    MessageBoxCritical = sp.SP_MessageBoxCritical
+    MessageBoxQuestion = sp.SP_MessageBoxQuestion
+    DockWidgetCloseButton = sp.SP_DockWidgetCloseButton
+    DesktopIcon = sp.SP_DesktopIcon
+    TrashIcon = sp.SP_TrashIcon
+    ComputerIcon = sp.SP_ComputerIcon
+    DriveFDIcon = sp.SP_DriveFDIcon
+    DriveHDIcon = sp.SP_DriveHDIcon
+    DriveCDIcon = sp.SP_DriveCDIcon
+    DriveDVDIcon = sp.SP_DriveDVDIcon
+    DriveNetIcon = sp.SP_DriveNetIcon
+    DirOpenIcon = sp.SP_DirOpenIcon
+    DirClosedIcon = sp.SP_DirClosedIcon
+    DirLinkIcon = sp.SP_DirLinkIcon
+    FileIcon = sp.SP_FileIcon
+    FileLinkIcon = sp.SP_FileLinkIcon
+    ToolBarHorizontalExtensionButton = sp.SP_ToolBarHorizontalExtensionButton
+    ToolBarVerticalExtensionButton = sp.SP_ToolBarVerticalExtensionButton
+    FileDialogStart = sp.SP_FileDialogStart
+    FileDialogEnd = sp.SP_FileDialogEnd
+    FileDialogToParent = sp.SP_FileDialogToParent
+    FileDialogNewFolder = sp.SP_FileDialogNewFolder
+    FileDialogDetailedView = sp.SP_FileDialogDetailedView
+    FileDialogInfoView = sp.SP_FileDialogInfoView
+    FileDialogContentsView = sp.SP_FileDialogContentsView
+    FileDialogListView = sp.SP_FileDialogListView
+    FileDialogBack = sp.SP_FileDialogBack
+    DirIcon = sp.SP_DirIcon
+    DialogOkButton = sp.SP_DialogOkButton
+    DialogCancelButton = sp.SP_DialogCancelButton
+    DialogHelpButton = sp.SP_DialogHelpButton
+    DialogOpenButton = sp.SP_DialogOpenButton
+    DialogSaveButton = sp.SP_DialogSaveButton
+    DialogCloseButton = sp.SP_DialogCloseButton
+    DialogApplyButton = sp.SP_DialogApplyButton
+    DialogResetButton = sp.SP_DialogResetButton
+    DialogDiscardButton = sp.SP_DialogDiscardButton
+    DialogYesButton = sp.SP_DialogYesButton
+    DialogNoButton = sp.SP_DialogNoButton
+    ArrowUp = sp.SP_ArrowUp
+    ArrowDown = sp.SP_ArrowDown
+    ArrowLeft = sp.SP_ArrowLeft
+    ArrowRight = sp.SP_ArrowRight
+    ArrowBack = sp.SP_ArrowBack
+    ArrowForward = sp.SP_ArrowForward
+    DirHomeIcon = sp.SP_DirHomeIcon
+    CommandLink = sp.SP_CommandLink
+    VistaShield = sp.SP_VistaShield
+    BrowserReload = sp.SP_BrowserReload
+    BrowserStop = sp.SP_BrowserStop
+    MediaPlay = sp.SP_MediaPlay
+    MediaStop = sp.SP_MediaStop
+    MediaPause = sp.SP_MediaPause
+    MediaSkipForward = sp.SP_MediaSkipForward
+    MediaSkipBackward = sp.SP_MediaSkipBackward
+    MediaSeekForward = sp.SP_MediaSeekForward
+    MediaSeekBackward = sp.SP_MediaSeekBackward
+    MediaVolume = sp.SP_MediaVolume
+    MediaVolumeMuted = sp.SP_MediaVolumeMuted
+    DirLinkOpenIcon = sp.SP_DirLinkOpenIcon
+    LineEditClearButton = sp.SP_LineEditClearButton
+    DialogYesToAllButton = sp.SP_DialogYesToAllButton
+    DialogNoToAllButton = sp.SP_DialogNoToAllButton
+    DialogSaveAllButton = sp.SP_DialogSaveAllButton
+    DialogAbortButton = sp.SP_DialogAbortButton
+    DialogRetryButton = sp.SP_DialogRetryButton
+    DialogIgnoreButton = sp.SP_DialogIgnoreButton
+    RestoreDefaultsButton = sp.SP_RestoreDefaultsButton
+    CustomBase = sp.SP_CustomBase
     # fmt: on
