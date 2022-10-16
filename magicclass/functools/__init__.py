df5c16f77778ee3ebff8eb0a4cdb060669e6d2b1
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2022-10-16 23:57:44 +0900

    add singledispatch

--- magicclass/functools/__init__.py
+++ magicclass/functools/__init__.py
@@ -2,6 +2,7 @@
 A magic-class submodule that mimics the built-in ``functools`` module.
 """
 
+from ._dispatch import singledispatch, singledispatchmethod
 from ._partial import partial, partialmethod
 from ._wraps import wraps
 
@@ -9,4 +10,7 @@ from ._wraps import wraps
 __all__ = [
     "partial",
     "partialmethod",
+    "singledispatch",
+    "singledispatchmethod",
+    "wraps",
 ]
