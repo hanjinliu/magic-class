ebe83b414b0330c624bc790514f07575e77424b5
Hanjin Liu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2023-04-08 17:38:57 +0900

    add doc

--- magicclass/ext/pyaudio/__init__.py
+++ magicclass/ext/pyaudio/__init__.py
@@ -9,3 +9,16 @@ __all__ = ["AudioRecorder", "AudioData"]
 AudioData = NewType("AudioData", NDArray[np.int16])
 
 register_type(AudioData, widget_type=AudioRecorder)
+
+__doc__ = """
+An extension submodule for audio input.
+
+Examples
+--------
+
+>>> from magicgui import magicgui
+>>> from magicclass.ext.pyaudio import AudioData
+>>> @magicgui
+>>> def foo(audio: AudioData):
+...     print(audio.shape)
+"""
