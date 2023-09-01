a281600124b519c6560e0218640f785401dddf63
Hanjin Liu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2023-09-01 18:15:41 +0900

    support if, for, try etc

--- magicclass/utils/qthreading/_to_async.py
+++ magicclass/utils/qthreading/_to_async.py
@@ -30,7 +30,7 @@ def _rewrite_callback(lines: list[Expr]) -> Expr:
     func_body = Expr(Head.block, lines)
     cb_expr = Symbol("__magicclass_temp_callback")
     funcdef = Expr(Head.function, [Expr(Head.call, [cb_expr]), func_body])
-    funcdef_dec = Expr(Head.decorator, [parse("thread_worker.callback"), funcdef])
+    funcdef_dec = Expr(Head.decorator, [DEC_CB, funcdef])
     cb_yield = Expr(Head.yield_, [cb_expr])
     cb_await = Expr(Head.call, [Expr(Head.getattr, [cb_expr, "await_call"])])
     return Expr(Head.block, [funcdef_dec, cb_yield, cb_await])
@@ -39,12 +39,12 @@ def _rewrite_callback(lines: list[Expr]) -> Expr:
 CAN_PASS = frozenset(
     [Head.yield_, Head.comment, Head.class_, Head.function, Head.decorator]
 )
+FORBIDDEN = frozenset([Head.import_, Head.from_])
 DECORATOR = parse("thread_worker(force_async=True)")
+DEC_CB = parse("thread_worker.callback")
 
 
-def to_async_code(code: Expr, ui: BaseGui) -> Expr:
-    """Convert the code to async-compatible code."""
-    assert code.head is Head.block
+def _to_async_code_list(code_lines: list[Symbol | Expr], ui: BaseGui) -> Expr:
     lines: list[Expr] = []
     stack: list[Expr] = []
 
@@ -55,16 +55,44 @@ def to_async_code(code: Expr, ui: BaseGui) -> Expr:
         return None
 
     ns = {str(ui._my_symbol): ui}
-    for line in code.args:
+    for line in code_lines:
+        if isinstance(line, Symbol):
+            lines.append(line)
         if _is_thread_worker_call(line, ns):
             _flush_stack()
             lines.append(_rewrite_thread_worker_call(line))
         elif line.head in CAN_PASS:
             lines.append(line)
+        elif line.head in FORBIDDEN:
+            raise ValueError(f"Cannot use {line.head} in async code: {line}")
+        elif line.head is Head.if_:
+            lines.append(_wrap_blocks(line, [1, 2], ui))
+        elif line.head in (Head.for_, Head.while_, Head.with_):
+            lines.append(_wrap_blocks(line, [1], ui))
+        elif line.head is Head.try_:
+            lines.append(_wrap_blocks(line, [0, 2, 3, 4], ui))
         else:
             stack.append(line)
     if stack:
         lines.append(_rewrite_callback(stack))
+    return lines
+
+
+def _wrap_blocks(line: Expr, idx: list[int], ui: BaseGui) -> Expr:
+    out = []
+    for i, arg in enumerate(line.args):
+        if i in idx:
+            blk = _to_async_code_list(arg.args, ui)
+            out.append(Expr(Head.block, blk))
+        else:
+            out.append(arg)
+    return Expr(line.head, out)
+
+
+def to_async_code(code: Expr, ui: BaseGui) -> Expr:
+    """Convert the code to async-compatible code."""
+    assert code.head is Head.block
+    lines = _to_async_code_list(code.args, ui)
     _fn = Symbol("_")
     func_body = Expr(Head.block, lines)
     funcdef = Expr(Head.function, [Expr(Head.call, [_fn, ui._my_symbol]), func_body])
