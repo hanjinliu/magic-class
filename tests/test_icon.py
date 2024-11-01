e2f02bbb12b19315a6ab1d2743a4c1ac8cd5c93a
Hanjin Liu <liuhanjin-sc@i.softbank.jp>
2024-11-01 23:02:20 +0900

    add test

--- tests/test_icon.py
+++ tests/test_icon.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+
+import pytest
 from magicclass import magicclass, magictoolbar, magicmenu, field, set_design
 from qtpy.QtGui import QIcon
 
@@ -6,15 +8,17 @@ def _icon_byte(a):
     icon: QIcon = a.native.icon()
     return icon.pixmap(10,10).toImage().sizeInBytes()
 
-PATH = Path(__file__).parent / "icons" / "star.png"
+PATH_STAR = Path(__file__).parent / "icons" / "star.png"
+PATH_TRIANGLE = Path(__file__).parent / "icons" / "triangle.svg"
 
-def test_icon():
+@pytest.mark.parametrize("icon_path", [PATH_STAR, PATH_TRIANGLE])
+def test_icon(icon_path: Path):
     @magicclass
     class A:
         @magicmenu
         class Menu:
-            a = field(bool, options={"icon": PATH})
-            @set_design(icon=PATH)
+            a = field(bool, options={"icon": icon_path})
+            @set_design(icon=icon_path)
             def func(self): ...
 
     ui = A()
@@ -26,8 +30,8 @@ def test_icon():
     class A:
         @magictoolbar
         class Menu:
-            a = field(bool, options={"icon": PATH})
-            @set_design(icon=PATH)
+            a = field(bool, options={"icon": icon_path})
+            @set_design(icon=icon_path)
             def func(self): ...
 
     ui = A()
@@ -35,15 +39,16 @@ def test_icon():
     assert _icon_byte(ui.Menu.a) > 0
     assert _icon_byte(ui.Menu["func"]) > 0
 
-def test_icon_in_class_construction():
-    @magicclass(icon=PATH)
+@pytest.mark.parametrize("icon_path", [PATH_STAR, PATH_TRIANGLE])
+def test_icon_in_class_construction(icon_path: Path):
+    @magicclass(icon=icon_path)
     class A:
         @magicmenu
         class Menu:
-            @magicmenu(icon=PATH)
+            @magicmenu(icon=icon_path)
             class X:
                 pass
 
     ui = A()
-    assert ui.native.windowIcon().pixmap(10,10).toImage().sizeInBytes() > 0
+    assert ui.native.windowIcon().pixmap(10, 10).toImage().sizeInBytes() > 0
     assert _icon_byte(ui.Menu.X) > 0
